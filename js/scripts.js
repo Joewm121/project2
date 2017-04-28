12f72d7e6e4777e31c181e6cff5697f3ed4bea71
joe werderber <greendog22@gmail.com>
2017-04-28 13:34:44 -0700

    add project1 content, add comment box, add toggle to panel title to display hidden paragraphs

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $("#panel-title").click(function() {
+    $("#aboutme").toggle();
+    $("#backround").toggle();
+  });
+});
+
+// UI logic
+$(function() {
+  $("#contact").submit(function(event) {
+    var name = $("input#name").val();
+    var email = $("input#email").val()
+    var comment = $("input#comment").val()
+    $(".nameConfirm").text(name);
+    $(".emailConfirm").text(email);
+    $(".commentConfirm").text(comment);
+    $(".container1").fadeToggle();
+    $(".container2").fadeToggle();
+    event.preventDefault();
+  });
+});
